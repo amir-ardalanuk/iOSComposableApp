7560a2a473b05e223c34635e466d5d0e76dbaf17
Faraz Karimi <faraz.karimi1990@gmail.com>
2021-11-18 13:51:06 +0330

    added theme

---
+++ REO/Resource/Theme.swift
@@ -0,0 +1,34 @@
+//
+//  Theme.swift
+//  REO
+//
+//  Created by Faraz Karimi on 8/27/1400 AP.
+//
+
+import Foundation
+import SwiftUI
+
+struct Theme {
+    var font: CustomFont
+}
+
+protocol CustomFont {
+    func regular(size: CGFloat) -> Font
+    func bold(size: CGFloat) -> Font
+}
+
+struct SystemFont: CustomFont {
+    func regular(size: CGFloat) -> Font {
+        Font.custom("Chalkboard-Bold", size: size)
+    }
+    
+    func bold(size: CGFloat) -> Font {
+        Font.custom("Chalkboard", size: size)
+    }
+}
+
+extension Theme {
+    static var global: Theme {
+        .init(font: SystemFont())
+    }
+}
