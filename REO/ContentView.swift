569395d85424a4b5ed80bf824b3f648b7a0560ba
Faraz Karimi <faraz.karimi1990@gmail.com>
2021-11-18 13:34:34 +0330

    init project

---
+++ REO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  REO
+//
+//  Created by Faraz Karimi on 8/27/1400 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ REO/REOApp.swift
@@ -0,0 +1,17 @@
+//
+//  REOApp.swift
+//  REO
+//
+//  Created by Faraz Karimi on 8/27/1400 AP.
+//
+
+import SwiftUI
+
+@main
+struct REOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
